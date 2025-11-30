eca93ac9255bb657226468266730a90971140fa9
Didericksen <ddidericksen@Macmini.home>
2025-11-30 16:33:53 -0700

    Make layout async for ClerkProvider

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,12 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
 export const metadata = {
-  title: "Oh! Beef Noodle Soup",
-  description: "Order ahead. Skip the wait.",
+  title: "Oh Beef Noodle Soup",
+  description: "Order your favorite noodle soup",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
